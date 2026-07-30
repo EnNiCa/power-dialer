0ef01369590f833157b41331f674cf9b05104e77
Enrique <enrniecam94@outlook.es>
2026-07-30 11:09:08 +0200

    Creada carpeta templates y login.html

--- auth/routes.py
+++ auth/routes.py
@@ -22,9 +22,9 @@ def login():
             session['rol'] = usuario['rol']
             return redirect(url_for('home'))
         else:
-            return render_template('login.html', error="Usuario o contraseña incorrectos")
+            return render_template('auth/login.html', error="Usuario o contraseña incorrectos")
         
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 @auth_bp.route('/logout')
 def logout():
